cb9f02f5b275bbac1fc807cb3815dae2b9040b2b
lakshya3190 <lakshya_tyagi@icloud.com>
2022-08-11 23:31:08 +0530

    Created new homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import 'tachyons';
-import {Animated} from "react-animated-css";
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,18 +9,10 @@ import {
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
-import About from './Components/About/about.js';
 import Home from './Components/Home/home.js';
-import Contact from './Components/Contact/contact.js';
-import Experience from './Components/Work/work.js';
-import CopeWithCovid from './Components/Work/copewithcovid/copewithcovid.js';
-import Journie from './Components/Work/journie/journie.js';
-import Research1 from './Components/Work/research1/research1.js';
-import Research2 from './Components/Work/research2/research2.js';
-import Research3 from './Components/Work/research3/research3.js';
 import Logo from './logo.png';
 import Trans from './transition.js';
-import Particles from 'react-particles-js';
+
 
 
 const trackingId = "UA-186285153-3";
@@ -39,11 +29,13 @@ class App extends React.Component {
 
   }
 
+  //Google analytics initizalization
   initGA = () => {
     ReactGA.initialize(trackingId);
     ReactGA.pageview(window.location.pathname + window.location.search);
   }
 
+  //Loader transition
   componentDidMount(){
     setTimeout(()=> this.setState({transitionOn: !this.state.transitionOn}), 2000)
     this.initGA();
@@ -59,168 +51,30 @@ class App extends React.Component {
 
   render(){
     return (
-      <div>
-        {
-          
-          this.state.transitionOn === true
-          ? <div>
-          <Animated animationIn="slideInDown" animationOut="slideOutDown" animationOutDelay={1500} animationOutDuration={1500} isVisible={this.state.transitionOn}>
-          <Trans/>
-          </Animated>
+      <div className = 'App'>
+
+        <div className = 'nav'>
+         <div className = 'nav-content'>
+          <div className = 'nav-point logo-name'>LakshyaTyagi.</div>
+          <div className = 'nav-point menu'>
+            <div className = 'nav-point dot'></div>
+            <div className = 'nav-point dot'></div>
+            <div className = 'nav-point dot'></div>
           </div>
-          :<div>
-        <Router>
-        <div className = 'nav-bar'>
-          <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
-            <p  className = 'logo-tag'>
-              <Link to = "/">
-                 LakshyaTyagi
-              </Link></p>
-          </Animated>
-
-              <div className = 'sub-nav-bar'>
-                <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={1200} animationOutDuration={1000} isVisible={true}>
-                  <p onClick = {this.handleClick} className = 'nav-op pointer dim'>
-                    <Link to = "/about" style={{ textDecoration: 'none', color: 'black' }}>
-                      About
-                    </Link>
-                  </p>
-                </Animated>
-
-                <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={1600} animationOutDuration={1000} isVisible={true}>
-                  <p onClick = {this.handleClick} className = 'nav-op pointer dim'>
-                    <Link to = "/experience" style={{ textDecoration: 'none', color: 'black' }}>
-                      Experience
-                    </Link>
-                  </p>
-                </Animated>
-
-                <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={1400} animationOutDuration={1000} isVisible={true}>
-                  <p className = 'nav-op pointer dim'>
-                    <a href = "https://warriorsway98.wordpress.com/">Blog</a>
-                  </p>
-                </Animated>
-
-
-                <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={1800} animationOutDuration={1000} isVisible={true}>
-                  <p className = 'nav-op pointer dim'>
-                    <a href = "https://drive.google.com/file/d/1wcXXoUbCOVUxXdrMTK1G34aEywEiEArO/view?usp=sharing">Resume</a>
-                  </p>
-                </Animated>
-
-                <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={2000} animationOutDuration={1000} isVisible={true}>
-                  <p onClick = {this.handleClick} className = 'nav-op pointer dim'>
-                    <Link to = "/contact" style={{ textDecoration: 'none', color: 'black' }}>
-                      Contact
-                    </Link>
-                  </p>
-                </Animated>
-
-                <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={1800} animationOutDuration={1000} isVisible={true}>
-                  <p className = 'nav-op pointer grow dim hide-op'>
-                    <Link to = "/work/copewithcovid" style={{ textDecoration: 'none', color: 'black' }}>
-                      CopeWithCovid
-                    </Link>
-                  </p>
-                </Animated>
-
-                <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={1800} animationOutDuration={1000} isVisible={true}>
-                  <p className = 'nav-op pointer grow dim hide-op'>
-                    <Link to = "/work/journie" style={{ textDecoration: 'none', color: 'black' }}>
-                      Journie
-                    </Link>
-                  </p>
-                </Animated>
-
-                <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={1800} animationOutDuration={1000} isVisible={true}>
-                  <p className = 'nav-op pointer grow dim hide-op'>
-                    <Link to = "/work/research1" style={{ textDecoration: 'none', color: 'black' }}>
-                      Research1
-                    </Link>
-                  </p>
-                </Animated>
-
-                <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={1800} animationOutDuration={1000} isVisible={true}>
-                  <p className = 'nav-op pointer grow dim hide-op'>
-                    <Link to = "/work/research1" style={{ textDecoration: 'none', color: 'black' }}>
-                      Research2
-                    </Link>
-                  </p>
-                </Animated>
-
-                <Animated animationIn="fadeInDown" animationOut="fadeInOut" animationInDuration={1800} animationOutDuration={1000} isVisible={true}>
-                  <p className = 'nav-op pointer grow dim hide-op'>
-                    <Link to = "/work/research3" style={{ textDecoration: 'none', color: 'black' }}>
-                      Research3
-                    </Link>
-                  </p>
-                </Animated>
-              </div>
-
-              <div className = 'res-nav-bar dropdown'>
-                <button className = 'dropbtn'>MENU</button>
-                <div className = 'dropdown-content'>
-                    <p onClick = {this.handleClick} className = 'pointer drop-op'><Link to = "/about" style={{ textDecoration: 'none', color: 'black' }}>About</Link></p>
-                    <p className = 'pointer drop-op' ><a href = "https://warriorsway98.wordpress.com/">Blog</a></p>
-                    <p onClick = {this.handleClick} className = 'pointer drop-op' ><Link to = "/experience" style={{ textDecoration: 'none', color: 'black' }}>Work</Link></p>
-                    <p className = 'pointer drop-op' ><a href = "https://drive.google.com/file/d/1wcXXoUbCOVUxXdrMTK1G34aEywEiEArO/view?usp=sharing">Resume</a></p>
-                    <p onClick = {this.handleClick} className = 'pointer drop-op' ><Link to = "/contact" style={{ textDecoration: 'none', color: 'black' }}>Contact</Link></p>
-
-                </div>
-              </div>  
-        
-        </div>          
-        <div>
-        
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/about">
-            <About />
-            </Route>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="/experience">
-              <Experience />
-            </Route>
-            <Route path="/work/copewithcovid">
-            
-              <CopeWithCovid />
-
-            </Route>
-            <Route path="/work/journie">
-              <Journie />
-            </Route>
-            <Route path="/work/research1">
-              <Research1 />
-            </Route>
-            <Route path="/work/research2">
-              <Research2 />
-            </Route>
-            <Route path="/work/research3">
-              <Research3 />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
+          <div className = 'nav-point logo-name'>VanGuard.</div>
+         </div>
         </div>
-      </Router>
+
+        <div className = 'view center'>
+          <Home/>
+        </div>
 
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&display=swap" rel="stylesheet"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet"></link>
-      </div>
 
-            
-        }
+
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@500&family=Poppins:wght@300&display=swap" rel="stylesheet"/>
       </div>
     );
   }
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'tachyons';
-import {Animated} from "react-animated-css";
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,123 +6,43 @@ import {
   Link
 } from "react-router-dom";
 import './home.css';
-import Particles from 'react-tsparticles';
-import Deer from './small-deer.svg';
 import "pathseg";
+import Shape from './threejs.js'
 
 
 class Home extends React.Component {
   constructor(props){
     super(props);
-    this.particlesInit = this.particlesInit.bind(this);
-    this.particlesLoaded = this.particlesLoaded.bind(this);
     this.state = {
       route: ''
     }
   }
 
-  particlesInit(main) {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  }
-
-  particlesLoaded(container) {
-    console.log(container);
-  }
-
   render(){
   return (
-    <div className="Home">
-  
-      <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDuration={2000} animationOutDuration={1000} isVisible={true}>
-        <div className = 'lp-body' style = {{display: 'flex', justifyContent: 'space-between'}}>
+    <div className="home">
+      
+      <div className = 'home-text center'>
+        Resilient. Driven. Bold.
+      <div className = 'home-text-sub'>
+        I build digital experiences.<br/>
+        Enable competence, merit and ethics.<br/>
+        Bridge technology with business outcomes.<br/>
+      </div>
+      <br/>
+      
 
-
-            <div className = 'name name-wrapper'>
-              <p className = 'name'>Lakshya<br/></p>
-              
-              <div className = 'name-p2' style = {{display: 'flex'}}>
-                <div>
-                  <ul className = 'desig-list'>
-                    <li>Writer</li>
-                    <li>Mentor</li>
-                    <li>Engineer</li>
-                    <li>Researcher</li>
-                  </ul>
-                </div>
-                <div class="vl"></div>
-                <div className = 'sname'>Tyagi<br/></div>
-                
-
-              </div>
-          </div>
-
-
-
-          <div className = 'projects'>
-              <div className = 'proj-unit'>
-                <p className = 'proj-name f6'>Experience</p>
-                <p className = 'proj-no pointer grow'><Link to = "/experience" style={{ textDecoration: 'none', color: 'black' }}>01</Link></p>
-              </div>
-
-              <div className = 'proj-unit'>
-                <p className = 'proj-name f6'>CopeWithCovid</p>
-                <p className = 'proj-no pointer grow'><Link to = "/work/copewithcovid" style={{ textDecoration: 'none', color: 'black' }}>02</Link></p>
-              </div>
-
-              <div className = 'proj-unit'>
-              <p className = 'proj-name f6'>Journie</p>
-              <p className = 'proj-no pointer grow'><Link to = "/work/journie" style={{ textDecoration: 'none', color: 'black' }}>03</Link></p>
-              </div>
-
-              <div className = 'proj-unit'>
-              <p className = 'proj-name f6'>Research I</p>
-              <p className = 'proj-no pointer grow'><Link to = "/work/research1" style={{ textDecoration: 'none', color: 'black' }}>04</Link></p>
-              </div>
-
-              <div className = 'proj-unit'>
-              <p className = 'proj-name f6'>Research II</p>
-              <p className = 'proj-no pointer grow proj-no-5'><Link to = "/work/research2" style={{ textDecoration: 'none'}}>05</Link></p>
-              </div>
-
-              <div className = 'proj-unit'>
-              <p className = 'proj-name f6'>Research III</p>
-              <p className = 'proj-no pointer grow'><Link to = "/work/research3" style={{ textDecoration: 'none', color: 'black' }}>06</Link></p>
-              </div>
-
-              <div className = 'proj-unit'>
-              <p className = 'proj-name f6'>Resume</p>
-              <p className = 'proj-no pointer grow'>07</p>
-              </div>
-
-              <div className = 'proj-unit'>
-              <p className = 'proj-name f6'>About</p>
-              <p className = 'proj-no pointer grow'><Link to = "/about" style={{ textDecoration: 'none', color: 'black' }}>08</Link></p>
-              </div>
-
-              <div className = 'proj-unit'>
-              <p className = 'proj-name f6'>Contact</p>
-              <p className = 'proj-no pointer grow'><Link to = "/contact" style={{ textDecoration: 'none', color: 'black' }}>09</Link></p>
-              </div>
-              
-          </div>
-
-        </div>
-      </Animated>
-      <div className = "hl"></div>
+      </div>
+      <div className='sphere center'>
+        <Shape/>
+      </div>
+    
 
           
       
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&display=swap" rel="stylesheet"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet"></link>
-        <script src="https://cdn.rawgit.com/progers/pathseg/master/pathseg.js"></script>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@500&display=swap" rel="stylesheet"></link>
     </div>
 
 
