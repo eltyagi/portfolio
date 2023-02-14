14ca717e9c1d6fd4942766a43a95c30a929433fc
ltyagi <ltyagi@cisco.com>
2023-02-14 15:49:53 +0530

    Updated animations for about page

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -1,52 +1,198 @@
 import React from 'react';
 import './about.css';
-import AboutMe from './aboutMe/aboutMe.js';
-import CurrentRole from './currentRole/currentRole.js';
-import GivingBack from './givingBack/givingBack.js';
+import Self from './self-img.png';
 import Research from './research/research.js';
 import {motion, useAnimation} from 'framer-motion';
 import {useInView} from 'react-intersection-observer';
-import { useEffect } from 'react';;
+import { useEffect } from 'react';
 
 const boxVariant = {
   visible: { opacity: 1, scale: 1, transition: { duration: 0.5 } },
   hidden: { opacity: 0, scale: 0 }
 };
 
-const aboutMe = () => {
+const AboutMe = () => {
 
   const control = useAnimation();
   const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
+  return(
+    <motion.div 
+    className = 'aboutMe'
+    ref={ref}
+    variants={boxVariant}
+    initial="hidden"
+    animate={control}
+    >
+      <div className = 'aboutMe-title'>
+        About Me
+      </div>
+      <div className = 'role'>
+        <div className = 'role-tag'>
+          About Me.
+          <br/><br/>
+        </div>
+
+        <div className = 'role-title'>
+          Focus. Resilience. Commitment.
+          <br/><br/>
+        </div>
+
+        <div className = 'role-desc'>
+          I am a solutions engineer, trusted technical advisor & developer who leverages 
+          automation and programmability to achieve business outcomes for Cisco's customers.
+          <br/><br/>
+          I am also a CSAP graduate, one of 136 ASEs and ASRs from Cisco's FY22
+          CSAP cohort, with deep exposure to security, collaboration, data center and
+          enterprise networking, the 4 pillars of any organization.
+        </div>
+
+        <div className = 'role-icons'>
+
+        </div>
+      </div>
+
+        <div className = 'role-image'>
+            <img width = '500px' src={Self}></img>
+        </div>
+                
+            
+
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet"/>      
+      </motion.div>
+  );
+}
+
+const CurrentRole = () => {
+
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
+  return(
+    <motion.div 
+    className = 'currentRole'
+    ref={ref}
+    variants={boxVariant}
+    initial="hidden"
+    animate={control}
+    >
+      <div className = 'currentRole-title'>
+        Current Role
+      </div>
+      <div className = 'role'>
+        <div className = 'role-tag'>
+          About Me.
+          <br/><br/>
+        </div>
+
+        <div className = 'role-title'>
+          Technical Solutions Specialist, GVE DevNet
+          <br/><br/>
+        </div>
+
+        <div className = 'role-desc'>
+          I am a solutions engineer, trusted technical advisor & developer who leverages 
+          automation and programmability to achieve business outcomes for Cisco's customers.
+          <br/><br/>
+          I am also a CSAP graduate, one of 136 ASEs and ASRs from Cisco's FY22
+          CSAP cohort, with deep exposure to security, collaboration, data center and
+          enterprise networking, the 4 pillars of any organization.
+        </div>
+
+        <div className = 'role-icons'>
+
+        </div>
+      </div>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet"/>      
+      </motion.div>
+  );
+}
+
+const GivingBack = () => {
+
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
+  return(
+    <motion.div 
+    className = 'currentRole'
+    ref={ref}
+    variants={boxVariant}
+    initial="hidden"
+    animate={control}
+    >
+      <div className = 'currentRole-title'>
+        Giving Back
+      </div>
+      <div className = 'role'>
+        <div className = 'role-tag'>
+          About Me.
+          <br/><br/>
+        </div>
+
+        <div className = 'role-title'>
+          Mentor, Machine Learing & Data Analytics
+          <br/><br/>
+        </div>
+
+        <div className = 'role-desc'>
+          I am a solutions engineer, trusted technical advisor & developer who leverages 
+          automation and programmability to achieve business outcomes for Cisco's customers.
+          <br/><br/>
+          I am also a CSAP graduate, one of 136 ASEs and ASRs from Cisco's FY22
+          CSAP cohort, with deep exposure to security, collaboration, data center and
+          enterprise networking, the 4 pillars of any organization.
+        </div>
+
+        <div className = 'role-icons'>
+
+        </div>
+      </div>
+
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet"/>      
+      </motion.div>
+  );
 }
 
 function About(){
-
-    const control = useAnimation()
-    const [ref, inView] = useInView()
-
-    const boxVariant = {
-      visible: {opacity: 1, scale: 1, transition: {duration: 1}},
-      hidden: {opacity: 0, scale: 0}
-    }
-
-    useEffect(() => {
-      if (inView){
-        control.start("visible")
-      }
-    }, [control,inView])
 
     return(
         <div className = 'about'>
          
           <div className = 'content-about'>
-            <motion.div
-              ref={ref} 
-              variants={boxVariant}
-              initial="hidden"
-              animate={control}
-              className = 'about-components'>
-            <AboutMe/>
-            </motion.div>
+            <div className = 'about-components'>
+              <AboutMe/>
+            </div>
             <div className = 'about-components'>
               <CurrentRole/>
             </div>
--- src/Components/About/aboutMe/aboutMe.js
+++ src/Components/About/aboutMe/aboutMe.js
@@ -7,43 +7,7 @@ const aboutMe= () => {
 
         return(
             <div className = 'aboutMe'>
-                <div className = 'aboutMe-title'>
-                    About Me
-                </div>
-                <div className = 'role'>
-                    <div className = 'role-tag'>
-                        About Me.
-                        <br/><br/>
-                    </div>
-
-                    <div className = 'role-title'>
-                        Focus. Resilience. Commitment.
-                        <br/><br/>
-                    </div>
-
-                    <div className = 'role-desc'>
-                        I am a solutions engineer, trusted technical advisor & developer who leverages 
-                        automation and programmability to achieve business outcomes for Cisco's customers.
-                        <br/><br/>
-                        I am also a CSAP graduate, one of 136 ASEs and ASRs from Cisco's FY22
-                        CSAP cohort, with deep exposure to security, collaboration, data center and
-                        enterprise networking, the 4 pillars of any organization.
-                    </div>
-
-                    <div className = 'role-icons'>
-
-                    </div>
-                </div>
-
-                <div className = 'role-image'>
-                    <img width = '500px' src={Self}></img>
-                </div>
-                
-            
-
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-            <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet"/>      
+               
             </div>
         );
 }
