addd7b56092ce1bde3ea938e83fec1d357754454
ltyagi <ltyagi@cisco.com>
2023-07-10 17:30:48 +0530

    Latest commit

--- src/Components/Home/background-trunk/background-trunk.js
+++ src/Components/Home/background-trunk/background-trunk.js
@@ -1,35 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import p5 from 'p5';
 import TRUNK from 'vanta/dist/vanta.trunk.min';
+import { useMediaQuery } from 'react-responsive'
+
 
 // Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
 
-class MyComponent extends React.Component {
-  constructor() {
-    super()
-    this.vantaRef = React.createRef()
-  }
-  componentDidMount() {
-    this.vantaEffect = TRUNK({
-      el: this.vantaRef.current,
-      p5: p5, // use a custom p5 when initializing
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.00,
-      minWidth: 200.00,
-      scale: 1.00,
-      scaleMobile: 1.00,
-      color: 0xffffff,
-      backgroundColor:0x1E1E1E
-    })
-  }
-  componentWillUnmount() {
-    if (this.vantaEffect) this.vantaEffect.destroy()
-  }
-  render() {
-    return <div style = {{width:'60vh', height:'35vw', backgroundColor:'#1E1E1E'}} ref={this.vantaRef}>
-    </div>
-  }
+function Trunk_component(){
+  const [spacingValue, setSpacingValue] = useState(0)
+  const [vantaEffect, setVantaEffect] = useState(null)
+  const myRef = useRef(null)
+
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(TRUNK({
+        el: myRef.current,
+        p5:p5,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0xffffff,
+        backgroundColor:0x1E1E1E,
+        spacing: spacingValue
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
+  return <div className = 'bg-trunk' style = {{width:'60vh', height:'35vw', backgroundColor:'#1E1E1E'}} ref={myRef}>
+  </div>
 }
-export default MyComponent;
+export default Trunk_component;
