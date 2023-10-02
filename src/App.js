55165b9c754d4e98267f39b3ffc7fc42d0bcb0a1
Lakshya Tyagi <lakshyatyagi@Lakshyas-MacBook-Pro.local>
2023-10-02 18:06:35 +0530

    Initial development of the landing page, added menu options and name

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link
-} from "react-router-dom";
-import { TransitionGroup, CSSTransition} from 'react-transition-group'
-import { useLocation } from 'react-router-dom'
-import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
-import Home from './Components/Home/home.js';
-import Trans from './transition.js';
-import {Animated} from "react-animated-css";
-import Contact from './Components/Contact/contact.js';
-import About from './Components/About/about.js';
-import logo from './logo_main.png'
+import LandingPage from './Components/newsletter_link/newsletter_link.js'
+
 
 
 const trackingId = "UA-186285153-3";
@@ -26,100 +12,52 @@ const trackingId = "UA-186285153-3";
 
 function App(){
 
-
     ReactGA.initialize(trackingId);
     ReactGA.pageview(window.location.pathname + window.location.search);
-  
-
-    const location = useLocation()
-    const [enableTransition, setEnableTransition] = useState(false)
     
-    useEffect(() => {
-      setEnableTransition(true);
-      setTimeout(() => {
-        setEnableTransition(false);
-      }, 2000);
-    }, []);
-
-    const onClickTransition = useCallback(async() => {
-      setEnableTransition(true);
-      setTimeout(() => {
-        setEnableTransition(false);
-      }, 2000);
-    }, [])
-    
-    return (
-
-
-      
+    return (      
       <div style = {{margin: "0px"}} className = 'App'>
-        {
-          enableTransition 
-          ?
-          (<div>
-            <CSSTransition
-              in = {enableTransition}
-              timeout = {1500}
-              classNames = "loaderTransition">
-              <Trans duration = {enableTransition}/>
-            </CSSTransition>
-          </div>)
-          :
-          (<div>
-            <div className = 'nav'>
-            
-              <div className = 'menu-top'>
-                <div>
-                    <Link to = "/" style={{ textDecoration: 'none', color: 'black' }}>
-                      <Animated animationInDelay = "100" animationInDuration = "1200" animationIn="fadeInDown" isVisible={true}>
-                        <img src = {logo} onClick = {onClickTransition} className = 'nav-point logo-name'/>
-                      </Animated>
-                    </Link>
-                  
-                </div>
+        <div className = 'menu-top'>
+          <div className = 'menu-op op-about'>
+            ABOUT
+          </div>
+        </div>
 
-                <div>
-                      <Link to = "/about" style={{ textDecoration: 'none', color: 'black' }}>
-                        <Animated animationInDelay = "100" animationInDuration = "1200" animationIn="fadeInDown" animationOut="fadeOut" isVisible={true}>
-                          <div  onClick = {onClickTransition} className = 'about-op'>ABOUT<br/><span className = 'op-reveal-responsive'>ABOUT</span></div>
-                        </Animated>
-                      </Link>
-                </div>
-              </div>
+        <div className = 'landing-page'>
+          <div className = 'first-name'>
+            Lakshya
+          </div>
+          <div className = 'last'>
+            <div className = 'last-name'>
+              Tyagi
+            </div>
 
-              <div className = 'menu-bottom'>
-                <div>
-                  <Animated animationInDelay = "100" animationInDuration = "1200" animationIn="fadeInUp" animationOut="fadeOut" isVisible={true}>
-                      <div className = 'blog-op'><a  style = {{textDecoration: 'None', color: "white"}} className = 'blog-link' href = "https://warriorsway98.wordpress.com/">BLOG</a><br/><span className = 'op-reveal-responsive'>BLOG</span></div>
-                  </Animated>
-                </div>
+            <div className = 'desc'>
+              Solutions Engineer @ Cisco <br/>
+              Educator @ MyCaptain
+              <div className='hr'></div>
+            </div>
 
-                <div>
-                  <Link to = "/contact" style={{ textDecoration: 'none', color: 'black' }}>
-                    <Animated animationInDelay = "100" animationInDuration = "1200" animationIn="fadeInUp" animationOut="fadeOut" isVisible={true}>
-                      <div onClick = {onClickTransition} className = 'contact-op'>CONTACT<br/><span className = 'op-reveal-responsive'>CONTACT</span></div>
-                    </Animated>
-                  </Link>
-                </div>
-              </div>
+            <div className = 'newsletter'>
               
+            </div>
           </div>
 
-            <TransitionGroup>
-              <CSSTransition timeout={30} classNames='fade' key={location.key}>
-                <Routes location={location} style = {{margin: "0px"}}>
-                  <Route style = {{margin: "0px"}} path="/contact" element={<Contact/>}/>
-                  <Route style = {{margin: "0px"}} path="/about" element={<About/>}/>
-                  <Route style = {{margin: "0px"}} path="/" element={<Home/>}/>
-                </Routes>
-              </CSSTransition>
-            </TransitionGroup>
-          </div>
-        )}
+        </div>
 
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-      <link href="https://fonts.googleapis.com/css2?family=Forum&display=swap" rel="stylesheet"/>
+        <div className = 'menu-bottom'>
+          <div className = 'menu-op op-blog'>
+            BLOG
+          </div>
+          <div className = 'menu-op op-contact'>
+            CONTACT
+          </div>
+        </div>
+    
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Khmer&display=swap" rel="stylesheet"/>
+
       </div>
     )
 }
---
+++ src/Components/newsletter_link/newsletter_link.js
@@ -0,0 +1,12 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import './newsletter_link.css';
+
+function LandingPage(){
+    return(
+        <div className = "home">
+            
+        </div>
+    );
+}
+
+export default LandingPage;
