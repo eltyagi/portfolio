2b941f14291dc1c70f206c6b14bb6beebceddd63
Lakshya Tyagi <lakshyatyagi@Lakshyas-MacBook-Pro.local>
2023-10-20 17:05:45 +0530

    Improvements to the about page

--- src/Components/about/about.js
+++ src/Components/about/about.js
@@ -1,11 +1,20 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import './about.css';
 import './about_content'
-
+import './about_content'
+import content from './about_content';
+import AboutCard from './card/about_card.js';
 function About(){
     return(
         <div className = 'about'>
-            About
+            <div className = 'list'>
+                {
+                    content.map((about_content) => {
+                        
+                        return <AboutCard className = 'about-card' title = {about_content.title} key = {about_content.id} body = {about_content.body}/>
+                    })
+                }
+            </div>
         </div>
     );
 }
---
+++ src/Components/about/card/about_card.js
@@ -0,0 +1,19 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import './about_card.css';
+
+function AboutCard(props){
+    return(
+        <div className = 'about-card'>
+            <div className = 'title'>
+                {props.title}
+            </div>
+            <div className = 'body'>
+                {props.body}
+            </div>
+
+         
+        </div>
+    );
+}
+
+export default AboutCard;
