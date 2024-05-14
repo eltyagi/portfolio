059f694bc8b3997c88aa9175770dd1695dc2ce84
Lakshya Tyagi <lakshyatyagi@Lakshyas-MacBook-Pro.local>
2024-05-14 17:25:42 +0530

    Improvements to jobs section

--- src/Components/landing_page/landing_page.js
+++ src/Components/landing_page/landing_page.js
@@ -72,12 +72,22 @@ function LandingPage(){
             </div>
 
             <div className = 'about-curr-roles'>
+
+            <div className = 'curr-role-1'>
+                <div className = 'role-name'>
+                  Solutions Engineer @ GitHub
+                </div>
+                <div className = 'role-date'>
+                  May 2024 - Present
+                </div>
+              </div>
+
               <div className = 'curr-role-1'>
                 <div className = 'role-name'>
                   Solutions Engineer @ Cisco
                 </div>
                 <div className = 'role-date'>
-                  Aug 2021 - Present
+                  Aug 2021 - May 2024
                 </div>
               </div>
 
